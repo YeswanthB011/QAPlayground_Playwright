e6e870ae15925fa04b3d198b73202d3e44586973
Yeswanth <104953452+YeswanthB011@users.noreply.github.com>
2025-04-10 14:14:55 +0530

    Update Interactions.ts

--- PageObjects/Interactions.ts
+++ PageObjects/Interactions.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test'
+import { Page, expect } from '@playwright/test'
 import path from 'path'
 
 
@@ -13,14 +13,40 @@ export class Interactions {
 
     async uploadFile() {
         await this.page.getByText('Upload File').click()
-        try{
-        this.page.locator('#file-input').setInputFiles('../Resources/image.png')
-        }catch{
+        try {
+            this.page.locator('#file-input').setInputFiles('../Resources/image.png')
+        } catch {
             console.log("coding issue")
         }
     }
 
-    async chainRedirection(){
-        await this.page()
+    async chainRedirection() {
+        this.page.getByText("Redirect Chain").click()
+        const secondpage = this.page.waitForResponse((response) =>
+            response.url().includes('second'))
+        const thirdpage = this.page.waitForResponse((response) =>
+            response.url().includes('third'))
+        const fourthpage = this.page.waitForResponse((response) =>
+            response.url().includes('fourth'))
+        const fifthpage = this.page.waitForResponse((response) =>
+            response.url().includes('fifth'))
+        const sixthpage = this.page.waitForResponse((response) =>
+            response.url().includes('sixth'))
+        const lastpage = this.page.waitForResponse((response) =>
+            response.url().includes('last'))
+
+
+        await this.page.locator('#redirect').click()
+
+        await secondpage
+        await thirdpage
+        await fourthpage
+        await fifthpage
+        await sixthpage
+        await lastpage
+        await this.page.waitForSelector('#info')
+        expect(this.page.locator('#info')).toBeVisible()
+
+
     }
 }
